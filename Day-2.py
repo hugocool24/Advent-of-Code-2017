a09c5959420526b889ec1ada6119410e3e292543
Hugo Sjöberg <hugo.sjoberg88@gmail.com>
2017-12-17 20:24:40 +0100

    Added solution 2 for day2

--- Day-2.py
+++ Day-2.py
@@ -4,10 +4,29 @@ results = []
 for line in f:
     line = list(map(int, line.split()))
     results.append(line)
-
 tot_diff = 0
 for line in results:
     maximum = max(line)
     minimum = min(line)
     tot_diff += maximum-minimum
 print (tot_diff)
+
+y = 0
+line = [[5,9,2,8],[9,4,7,3],[3,8,6,5]]
+
+def tester(matrix,number,memory):
+    for x in matrix[number]:
+        placeholder = list(matrix[number])
+        placeholder.remove(x)
+        for y in placeholder:
+            try:
+                if x%y == 0:
+                    memory += x/y
+            except:
+                pass
+    if number > 0:
+        return tester(matrix,number-1,memory)
+    else:
+        return memory
+
+print(tester(results,len(results)-1,0))
